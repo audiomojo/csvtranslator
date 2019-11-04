303a6e4df3d155c8d9e935e96896036d5dbc9c91
audiomojo <audiomojo@yahoo.com>
2019-11-04 09:43:54 -0700

    Added Service and first Unit Tests

--- src/main/java/com/softwarecraftsmanship/csvtranslator/controllers/CSVTranslatorController.java
+++ src/main/java/com/softwarecraftsmanship/csvtranslator/controllers/CSVTranslatorController.java
@@ -1,4 +1,19 @@
 package com.softwarecraftsmanship.csvtranslator.controllers;
 
+import com.softwarecraftsmanship.csvtranslator.services.CSVTranslatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CSVTranslatorController {
+
+    @Autowired
+    CSVTranslatorService csvTranslatorService;
+
+    @RequestMapping("/translate")
+    public String translate(@RequestParam (value="csv") String csv){
+        return csvTranslatorService.translate(csv);
+    }
 }
---
+++ src/main/java/com/softwarecraftsmanship/csvtranslator/services/CSVTranslatorService.java
@@ -0,0 +1,10 @@
+package com.softwarecraftsmanship.csvtranslator.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CSVTranslatorService {
+    public String translate(String csv) {
+        return csv;
+    }
+}
